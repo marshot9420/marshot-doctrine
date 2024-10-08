179707244125a05c1996652716fdbc61f8f077f7
marshot <124557537+marshot9420@users.noreply.github.com>
2024-10-08 18:16:52 +0900

    feat: 레이아웃 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,31 @@
-import "../styles/globals.css";
+import '../styles/globals.css'
 
-import type { Metadata } from "next";
+import type { Metadata } from 'next'
+
+import { Footer, Header } from '@/components'
+import { APP } from '@/constants'
 
 export const metadata: Metadata = {
-  title: "MARSHOT DOCTRINE",
-  description: "MARSHOT의 개인 블로그",
-};
+  title: APP.META.TITLE,
+  description: APP.META.DESCRIPTION,
+}
 
 interface RootLayoutProps {
-  children: React.ReactNode;
+  children: React.ReactNode
 }
 
 const RootLayout = ({ children }: RootLayoutProps) => {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body>
+        <div className="flex flex-col min-h-inherit">
+          <Header />
+          <main className="mt-[5.6rem] flex grow flex-col">{children}</main>
+          <Footer />
+        </div>
+      </body>
     </html>
-  );
-};
+  )
+}
 
-export default RootLayout;
+export default RootLayout
