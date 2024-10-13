9859b53704f9f6f4b5bb2cc8c426f614675615eb
marshot <124557537+marshot9420@users.noreply.github.com>
2024-10-14 02:17:40 +0900

    feat: `UserMenu` 로그인 상태 전용 메뉴 노출

--- src/app/(admin)/_components/layouts/AdminHeader/UserMenu/UserMenu.tsx
+++ src/app/(admin)/_components/layouts/AdminHeader/UserMenu/UserMenu.tsx
@@ -1,7 +1,16 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+
+import { UserRole } from '@prisma/client'
 import clsx from 'clsx'
+import { useCallback } from 'react'
 
 import { TransparentButton } from '@/components'
 import { URLS } from '@/constants'
+import { useUserStore } from '@/store'
+
+import { logoutAction } from '@/actions/auth'
 
 interface UserMenuProps {
   isSidebar?: boolean
@@ -10,6 +19,15 @@ interface UserMenuProps {
 }
 
 const UserMenu = ({ isSidebar = false, active = false }: UserMenuProps) => {
+  const { username, role, clearUser } = useUserStore()
+  const router = useRouter()
+
+  const handleLogout = useCallback(async () => {
+    await logoutAction()
+    clearUser()
+    router.push(URLS.CLIENT.HOME)
+  }, [clearUser, router])
+
   return (
     <ul
       className={clsx(
@@ -19,7 +37,16 @@ const UserMenu = ({ isSidebar = false, active = false }: UserMenuProps) => {
       )}
     >
       <TransparentButton href={URLS.CLIENT.HOME} label="블로그" active={active} />
-      <TransparentButton href={URLS.ADMIN.LOGIN} label="로그인" active={active} />
+      {username ? (
+        <>
+          {role === UserRole.MARSHOT && (
+            <TransparentButton href={URLS.ADMIN.HOME} label="관리자" active={active} />
+          )}
+          <TransparentButton onClick={handleLogout} label="로그아웃" active={active} />
+        </>
+      ) : (
+        <TransparentButton href={URLS.ADMIN.LOGIN} label="로그인" active={active} />
+      )}
     </ul>
   )
 }
